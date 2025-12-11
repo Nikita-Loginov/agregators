99d890bf0fd1b9c9eed88062d6b77ad56f76ef21
Nikita <nikitaloginov264@gmail.com>
2025-12-11 16:00:17 +0300

    продолжение верстки страницы article

--- js/page.js
+++ js/page.js
@@ -4,18 +4,28 @@ import { toggleAccordeonItems } from "./modules/accordeon.js";
 import { initModal, checkStartOpen } from "./modules/modal.js";
 import { checkScrollY } from "./modules/header.js";
 import { initMenu } from "./modules/menu.js";
+import { initFormValidation } from "./modules/validate.js";
 
 const handleGlobalClick = (e) => {
   toggleDropdown(e);
   toggleAccordeonItems(e);
   initModal(e);
-  initMenu(e)
+  initMenu(e);
+};
+
+const initValidate = () => {
+  const forms = document.querySelectorAll("form.form");
+
+  forms.forEach((form) => {
+    initFormValidation(form);
+  });
 };
 
 document.addEventListener("DOMContentLoaded", () => {
-  checkStartOpen()
+  initValidate();
+  checkStartOpen();
   getHeightHeader();
-  firstActiveText()
+  firstActiveText();
 
   document.addEventListener("click", handleGlobalClick);
 });
